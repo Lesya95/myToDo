4e09f88acd8cb225b6f8c227bbfc76a89db3d68c
Lesya95 <super.ktcz0804@ya.ru>
2020-04-01 18:16:08 +0300

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,59 @@
-const { watch, src, dest , series} = require('gulp');
-
+const { watch, src, dest, series } = require('gulp');
+//переименование файлов
 const rename = require('gulp-rename')
-
+const cssmin = require('gulp-cssmin');
+// соединение файлов
 const concat = require('gulp-concat')
+
+//все итоговые файлы у нас уходят в папку билд
+
 
 function copyHtml() {
   return src('*.html')
     .pipe(dest('build/'));
 }
 
-function copyCss() {
-    return src(['node_modules/bulma/css/*.css', 'node_modules\font-awesome\css\font-awesome.css'])
-        .pipe(concat('style.css'))
-        .pipe(rename({suffix:".min"}))
-        .pipe(dest('build/'))
+function copyCss(){
+    // подтянуть несколько css файлов, сжать, переименовать, и положить в итоговый билд
+    return src(['node_modules/bulma/css/bulma.css', 'node_modules/font-awesome/css/font-awesome.css'])
+    .pipe(cssmin())
+    .pipe(concat('style.css'))
+    .pipe(rename({suffix:".min"}))
+    .pipe(dest('build/css'));
 }
 
-function getJs() {
-    return src('dist/*.js')
-      .pipe(dest('build/'));
+
+function getFonts(){
+    return src('node_modules/font-awesome/fonts/*.*')
+    .pipe(dest('build/fonts'));
+
+
 }
 
-exports.build = series(copyHtml, copyCss, getJs);
+function getJs(){
+return src('dist/*.js')
+    .pipe(dest('build/'));
+}
 
-function getReady() {
+
+exports.build = series(copyHtml, copyCss, getJs, getFonts);
+
+function getReady(){
     copyHtml()
     copyCss()
     getJs()
+    getFonts()
 }
 
-exports.default = function() {
-    getReady()
 
+exports.default = function() {
+    // You can use a single task
+    getReady()
     watch('*.html', copyHtml);
+
     watch('dist/*.js', getJs);
-    watch(['dist/*.js', '*.html'], series(copyHtml, getJs));
+
+
+    // Or a composed task
+    // watch(['dist/*.js', '*.html'], series(copyHtml, copyCss, getJs));
   };
